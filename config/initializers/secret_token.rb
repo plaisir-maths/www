5f6d09bb69cd98087e48a363ea198a70102d84d3
Yann Hourdel <yann.hourdel@gmail.com>
2012-09-11 10:14:52 +0200

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PlaisirMaths::Application.config.secret_token = '4cacbb20d4c1ade6d586b7b5c7d9e1b41f1c05df6c7955b5c0101d64284f5571c7b7df3c9b57e5422f1fe7fe95c5aed0ad022ef239aa9a65924c597023f704eb'
