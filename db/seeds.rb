74f4ff4092e256d6f679ab01f2c0d06e014f3eb2
Laura Pallez <laura.pallez@plaisir-maths.fr>
2012-09-15 19:08:23 +0200

    google analytics

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,6 @@ Refinery::Pages::Engine.load_seed
 
 # Added by Refinery CMS Portfolio Engine
 Refinery::Portfolio::Engine.load_seed
+
+# Added by Refinery CMS Blog engine
+Refinery::Blog::Engine.load_seed
